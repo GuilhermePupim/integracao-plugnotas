bac6732c4ebaea6ba4e9afe3f202abb7982795e4
Guilherme Pupim <guilherme.hpupim@gmail.com>
2025-04-08 08:57:11 -0300

    Projeto integracao-plugnotas final

--- src/app.js
+++ src/app.js
@@ -5,21 +5,20 @@ const routes = require('./api/routes');
 
 const app = express();
 
-// Middlewares
 app.use(express.json());
 app.use('/api', routes);
 
-// Conexão com o MongoDB
+
 console.log('AWS Region:', process.env.AWS_REGION);
 console.log('SQS Queue URL:', process.env.SQS_QUEUE_URL);
 
 mongoose.connect(process.env.MONGO_URI, {
-  serverSelectionTimeoutMS: 10000 // opcional: timeout de 10 segundos
+  serverSelectionTimeoutMS: 10000 
 })
   .then(() => {
     console.log('MongoDB conectado');
     
-    // Inicia a API após conectar no banco
+    
     const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
       console.log(`API rodando na porta ${PORT}`);
