d157b5b4884743e4998abe42457951db1fc26373
GuilhermePupim <92956548+GuilhermePupim@users.noreply.github.com>
2025-04-13 19:15:55 -0300

    Update viaCepService.js

--- src/services/viaCepService.js
+++ src/services/viaCepService.js
@@ -1,30 +1,30 @@
 const axios = require('axios');
 
-// Função para validar e formatar o CEP
+
 function formatCep(cep) {
-  // Remove qualquer caracter não numérico
-  return cep.replace(/\D/g, '');  // Mantém apenas os números
+  
+  return cep.replace(/\D/g, '');  
 }
 
-// Função para buscar os dados do CEP
+
 async function getCepData(cep) {
-  // Formata o CEP para garantir que esteja no formato correto (apenas números)
+  
   const formattedCep = formatCep(cep);
 
-  // Verifica se o CEP tem o comprimento correto (8 dígitos)
+  
   if (formattedCep.length !== 8) {
     throw new Error('CEP inválido. O CEP deve ter exatamente 8 números.');
   }
 
-  // Faz a requisição para o ViaCEP
+  
   const response = await axios.get(`https://viacep.com.br/ws/${formattedCep}/json/`);
 
-  // Se o ViaCEP retornar erro (ex: CEP não encontrado)
+  
   if (response.data.erro) {
     throw new Error('CEP inválido');
   }
 
-  // Retorna os dados do endereço do CEP
+  
   return response.data;
 }
 
